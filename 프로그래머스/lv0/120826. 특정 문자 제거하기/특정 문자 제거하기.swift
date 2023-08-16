cc6fe11e97623a93f6a368a7974d3c4ab5250501
HyunSoo-Lee <mm083434@gmail.com>
2023-08-17 08:34:40 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.02 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.swift
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.swift
@@ -2,8 +2,8 @@ import Foundation
 
 func solution(_ my_string:String, _ letter:String) -> String {
     var answer = ""
-    for i in my_string where String(i) != String(letter) {
-        answer += String(i)
+    for i in my_string where String(i) != letter {
+        answer.append(i)
     }
     return answer
 }
