02c34f967c49bbcd842974e0d258869fb23759f2
HyunSoo-Lee <mm083434@gmail.com>
2023-09-01 18:31:23 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.01 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+func solution(_ n:Int) -> Int {
+    lcm (n,6) / 6
+}
+
+func gcd(_ a: Int, _ b: Int) -> Int {
+    if b == 0 {
+        return a
+    } else {
+        return gcd(b, a % b)
+    }
+}
+
+func lcm(_ a: Int, _ b: Int) -> Int {
+    return a * b / gcd(a, b)
+}
