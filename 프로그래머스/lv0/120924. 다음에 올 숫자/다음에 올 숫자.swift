0a77513251b19fb150a28152aa638a21f8f83439
HyunSoo-Lee <mm083434@gmail.com>
2023-10-03 19:28:55 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.01 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.swift
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.swift
@@ -1,10 +1,7 @@
 import Foundation
 
 func solution(_ common:[Int]) -> Int {
-    let flag = common[2] - common[1] == common[1] - common[0] ? true : false
-    if flag {
-        return common[common.count-1] + common[2] - common[1]
-    } else {
-        return common[common.count-1] * common[2] / common[1]
-    }
+    return common[2] - common[1] == common[1] - common[0] ? 
+    common[common.count-1] + common[2] - common[1] :
+    common[common.count-1] * common[2] / common[1]
 }
