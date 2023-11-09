ec9a72c328fecc251beb550b015fbda471ea0129
HyunSoo-Lee <mm083434@gmail.com>
2023-11-09 18:30:34 +0900

    [Bronze II] Title: Hashing, Time: 8 ms, Memory: 69100 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.swift
+++ 백준/Bronze/15829. Hashing/Hashing.swift
@@ -1,12 +1,10 @@
-import Foundation
-let N = readLine()!
-let input = readLine()!
-var sum = 0
-var count: Double = 0
-let M = 1234567891
-for i in input {
-    let v = Int(UnicodeScalar(String(i))!.value) - 96
-    sum += v * Int(pow(31.0,count))
-    count += 1
+let mod = 1234567891
+readLine()
+let input = Array(readLine()!).map{Int(exactly: $0.asciiValue!)! - 96}
+var hash = 0
+var m = 1
+for i in input{
+    hash = (hash + i*m) % mod
+    m = (m*31) % mod
 }
-print(Int(sum) % M)
+print(hash)
