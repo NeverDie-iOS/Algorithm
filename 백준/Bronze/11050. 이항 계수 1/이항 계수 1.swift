837ba22f3525767a38b0eed42dcccaacd5cbea3e
HyunSoo-Lee <mm083434@gmail.com>
2023-11-11 00:58:25 +0900

    [Bronze I] Title: 이항 계수 1, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.swift
@@ -0,0 +1,10 @@
+let input = readLine()!.split(separator: " ").map{Int($0)!}
+print(fac(input[0]) / (fac(input[0]-input[1]) * fac(input[1])))
+
+func fac(_ x: Int) -> Int{
+    if x <= 1{
+        return 1
+    }
+    
+    return x * fac(x-1)
+}
