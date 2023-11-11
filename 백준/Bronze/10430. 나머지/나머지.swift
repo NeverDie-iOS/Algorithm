640b808c1e2ef7309472a637e2737c06660bd784
HyunSoo-Lee <mm083434@gmail.com>
2023-11-12 01:01:46 +0900

    [Bronze V] Title: 나머지, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.swift
@@ -0,0 +1,8 @@
+let input = readLine()!.split(separator: " ").map{Int(String($0))!}
+let A = input[0]
+let B = input[1]
+let C = input[2]
+print((A+B)%C)
+print(((A%C) + (B%C))%C)
+print((A*B)%C)
+print(((A%C) * (B%C))%C)
