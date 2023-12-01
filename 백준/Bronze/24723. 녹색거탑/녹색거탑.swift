49b9049831e92245e3cbfe7919c83bd4f02186f2
HyunSoo-Lee <mm083434@gmail.com>
2023-12-01 14:44:51 +0900

    [Bronze IV] Title: 녹색거탑, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Bronze/24723. 녹색거탑/녹색거탑.swift
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.swift
@@ -1,10 +1,6 @@
 let N = Int(readLine()!)!
-if N == 1 {
-    print(2)
-} else {
-    var result = 1
-    for i in 1...N {
-        result *= 2
+var result = 1
+for i in 1...N {
+    result *= 2
     }
-    print(result)
-}
+ print(result)
