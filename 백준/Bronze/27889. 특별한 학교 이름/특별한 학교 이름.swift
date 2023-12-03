35cddb5ca0e72e60e13f01017fcda10cd89cd2cc
HyunSoo-Lee <mm083434@gmail.com>
2023-12-04 00:18:41 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 8 ms, Memory: 69096 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.swift
@@ -0,0 +1,13 @@
+let input = readLine()
+switch input {
+    case "NLCS" :
+        print("North London Collegiate School")
+    case "BHA" :
+        print("Branksome Hall Asia")
+    case "KIS" :
+        print("Korea International School")
+    case "SJA" :
+        print("St. Johnsbury Academy")
+    default :
+        break
+}
