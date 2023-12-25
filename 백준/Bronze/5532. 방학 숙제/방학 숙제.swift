98b5c8dbddaa95cc4610610cb77704b3c50b7af9
HyunSoo-Lee <mm083434@gmail.com>
2023-12-25 14:14:28 +0900

    [Bronze IV] Title: 방학 숙제, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Bronze/5532. 방학 숙제/방학 숙제.swift
@@ -0,0 +1,6 @@
+var L = Int(readLine()!)!
+var A = Int(readLine()!)!
+var B = Int(readLine()!)!
+var C = Int(readLine()!)!
+var D = Int(readLine()!)!
+print(L - max((A - 1) / C + 1 , (B - 1) / D + 1))
