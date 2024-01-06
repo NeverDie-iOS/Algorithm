7094f00460896b54b0a0036c69fb4dd3d555eb6c
HyunSoo-Lee <mm083434@gmail.com>
2024-01-07 01:33:34 +0900

    [level 1] Title: 과일 장수, Time: 0.12 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.swift
@@ -0,0 +1,5 @@
+func solution(_ k:Int, _ m:Int, _ score:[Int]) -> Int {
+    let s = score.sorted(by: >)
+    return stride(from: m-1, to: score.count, by: m)
+        .reduce(0) { $0 + s[$1] * m }
+}
