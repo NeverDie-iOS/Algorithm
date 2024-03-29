d807cc6878a987166033be56e0e1a5d3cd485aca
HyunSoo-Lee <mm083434@gmail.com>
2024-03-29 17:52:00 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 12 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.swift
@@ -0,0 +1,22 @@
+let n = Int(readLine()!)!
+for _ in 1...n {
+    let input = readLine()!
+    switch input {
+    case "Algorithm" :
+        print("204")
+    case "DataAnalysis" :
+        print("207")
+    case "ArtificialIntelligence" :
+        print("302")
+    case "CyberSecurity" :
+        print("B101")
+    case "Network" :
+        print("303")
+    case "Startup" :
+        print("501")
+    case "TestStrategy" :
+        print(105)
+    default :
+        break
+    }
+}
